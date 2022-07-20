5398799abc6bcf296eb3aa7e31ce332c9be74536
Phạm Quang Sáng <phamquangsang661@gmail.com>
2022-07-20 19:53:43 +0700

    bai 1 a

---
+++ buoi19/script-2.js
@@ -0,0 +1,51 @@
+const students = []
+
+// Thêm SV
+
+//Để lấy html node theo id
+const getInputById = (id) => {
+    return document.getElementById(id)
+}
+
+//Để lấy giá trị theo id , hàm fc là hàm convert nếu có
+const getValueById = (id, fc = a => a) => {
+    return fc(document.getElementById(id).value)
+}
+
+
+// Dùng để chuẩn hóa tên
+const convertName = (name) => {
+    return name.split(' ').filter(Boolean).map(i => i[0].toUpperCase() + i.slice(1).toLowerCase()).join(' ')
+}
+
+const onClickAddStudent = () => {
+    const name = getValueById('inputName')
+    const age = getValueById('inputAge', parseInt)
+    const address = getValueById('inputAddress')
+
+    if (name === '' || age === null || address === '') {
+        alert("Không được để trống 1 trong 3 trường")
+        return
+    }
+    
+    students.push({
+        name,
+        age,
+        address
+    })
+}
+
+const onClickShowStudent = () => {
+    const listStudentNode = getInputById('student_list')
+    // Xóa hết tất cả student ở list cũ
+    while (listStudentNode.firstChild) {
+        listStudentNode.removeChild(listStudentNode.firstChild);
+    }
+    // Duyệt từng học sinh trong danh sách
+    students.forEach(student => {
+        const studentNode = document.createElement('div')
+        const { name, address, age } = student
+        studentNode.innerHTML = `Tên: ${name} - Tuổi: ${age} - Địa chỉ:${address}`
+        listStudentNode.appendChild(studentNode)
+    })
+}
