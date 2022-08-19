ba7da30514ae0506a8c09e1284b24f779853ed9a
Phạm Quang Sáng <phamquangsang661@gmail.com>
2022-08-19 19:49:55 +0700

    buoi 2 round 2

---
+++ round2/buoi2/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: ["./**/*.{html,js}"],
+    theme: {
+        extend: {
+            colors: {
+                sang: '#D96E8B'
+            }
+        },
+    },
+    plugins: [],
+}
